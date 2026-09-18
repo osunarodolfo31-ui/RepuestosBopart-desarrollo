d8271b3819e889b37b085f76fb7dcd4cd0aa3ae0
osunarodolfo31-ui <osunarodolfo31@gmail.com>
2026-09-18 13:29:31 -0400

    Conectar la web al servidor exclusivo de pruebas

--- dev-config.js
+++ dev-config.js
@@ -2,5 +2,5 @@
 window.BOPARTS_DEV_CONFIG = Object.freeze({
   environment: 'development',
   spreadsheetId: '1UUBQL2b1V5G5W6MFiFSjXttBDXP7oGG3q-6UvWKKKqo',
-  apiUrl: '' // Se completa con la nueva implementación /exec de DESARROLLO.
+  apiUrl: 'https://script.google.com/macros/s/AKfycbx4jedrpK6ZSKh_CWl9B4fbbpaUBkHHwRqLJ-9KUKl9N7lBYQcWDxE9wMyM4T4r0xQ30A/exec'
 });
